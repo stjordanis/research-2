d3643725e45e98b058aa08bd0bf19e7e882d7d6f
Skalman <skalman@riseup.net>
2019-02-20 14:10:20 +0100

    Deployed 4b7396a with MkDocs version: 1.0.4

--- mathjax-config.js
+++ mathjax-config.js
@@ -1,3 +1,8 @@
 MathJax.Hub.Config({
-  tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}
+  "tex2jax": {inlineMath: [['$','$'], ['\\(','\\)']]}
+});
+MathJax.Hub.Config({
+  config: ["MMLorHTML.js"],
+  jax: ["input/TeX", "output/HTML-CSS", "output/NativeMML"],
+  extensions: ["MathMenu.js", "MathZoom.js"]
 });
